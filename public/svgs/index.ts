2760326ddd615cc4e91fac74f18bdb4f869fef42
bro <gudgudgud714@naver.com>
2023-10-26 14:11:10 +0900

    svg 파일 추가

---
+++ public/svgs/index.ts
@@ -0,0 +1,25 @@
+import IconAlert from "./alert.svg";
+import IconArrowUp from "./arrow-up.svg";
+import IconArrowLeft from "./arrow-left.svg";
+import IconComment from "./comment.svg";
+import IconHome from "./home.svg";
+import IconSearch from "./search.svg";
+import IconUserCircle from "./user-circle.svg";
+import IconX_Circle from "./x-circle.svg";
+import IconX_Mark from "./x-mark.svg";
+import IconCogTooth from "./cog-8-tooth.svg";
+import IconChevronRight from "./chevron-right.svg";
+
+export {
+  IconAlert,
+  IconArrowUp,
+  IconArrowLeft,
+  IconComment,
+  IconHome,
+  IconSearch,
+  IconUserCircle,
+  IconX_Circle,
+  IconX_Mark,
+  IconCogTooth,
+  IconChevronRight,
+};
